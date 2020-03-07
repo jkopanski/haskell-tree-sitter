02742e660c3ee111d2e3d9574ff3816f7ef14b85
Jakub Kopański <jakub@famisoft.pl>
2020-03-07 22:53:33 +0100

    Use niv

--- release.nix
+++ release.nix
@@ -1,42 +1,58 @@
-{ withHoogle ? true
+{ sources ? import ./nix/sources.nix
+, withHoogle ? false
 }:
 let
-  pinnedPkgs = (import ./nix/nix-ghc-ptr.nix).pinnedPkgs;
-  hsLib = pinnedPkgs.haskell.lib;
-
-  customHaskellPackages = pinnedPkgs.haskellPackages.override (old: {
-    overrides = pinnedPkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
-      tree-sitter = self.callCabal2nix "tree-sitter" ./tree-sitter {};
-      tree-sitter-go = self.callCabal2nix "tree-sitter-go" ./tree-sitter-go {};
-      tree-sitter-haskell = self.callCabal2nix "tree-sitter-haskell" ./tree-sitter-haskell {};
-      tree-sitter-java = self.callCabal2nix "tree-sitter-java" ./tree-sitter-java {};
-      tree-sitter-json = self.callCabal2nix "tree-sitter-json" ./tree-sitter-json {};
-      tree-sitter-nix = self.callCabal2nix "tree-sitter-nix" ./tree-sitter-nix {};
-      tree-sitter-php = self.callCabal2nix "tree-sitter-php" ./tree-sitter-php {};
-      tree-sitter-python = self.callCabal2nix "tree-sitter-python" ./tree-sitter-python {};
-      tree-sitter-ruby = self.callCabal2nix "tree-sitter-ruby" ./tree-sitter-ruby {};
-      tree-sitter-tsx = self.callCabal2nix "tree-sitter-tsx" ./tree-sitter-tsx {};
-      tree-sitter-typescript = self.callCabal2nix "tree-sitter-typescript" ./tree-sitter-typescript {};
-      tree-sitter-verilog = self.callCabal2nix "tree-sitter-verilog" ./tree-sitter-verilog {};
+  packages = final: prev: {
+    haskellPackages = prev.haskellPackages.override (old: {
+      overrides = final.lib.composeExtensions
+        (old.overrides or (_: _: {}))
+        (self: super: {
+          tree-sitter = self.callCabal2nix "tree-sitter" ./tree-sitter {};
+          tree-sitter-go = self.callCabal2nix "tree-sitter-go" ./tree-sitter-go {};
+          tree-sitter-haskell = self.callCabal2nix "tree-sitter-haskell" ./tree-sitter-haskell {};
+          tree-sitter-java = self.callCabal2nix "tree-sitter-java" ./tree-sitter-java {};
+          tree-sitter-json = self.callCabal2nix "tree-sitter-json" ./tree-sitter-json {};
+          tree-sitter-nix = self.callCabal2nix "tree-sitter-nix" ./tree-sitter-nix {};
+          tree-sitter-php = self.callCabal2nix "tree-sitter-php" ./tree-sitter-php {};
+          tree-sitter-python = self.callCabal2nix "tree-sitter-python" ./tree-sitter-python {};
+          tree-sitter-ruby = self.callCabal2nix "tree-sitter-ruby" ./tree-sitter-ruby {};
+          tree-sitter-tsx = self.callCabal2nix "tree-sitter-tsx" ./tree-sitter-tsx {};
+          tree-sitter-typescript = self.callCabal2nix "tree-sitter-typescript" ./tree-sitter-typescript {};
+          tree-sitter-ql = self.callCabal2nix "tree-sitter-ql" ./tree-sitter-ql {};
+          tree-sitter-verilog = self.callCabal2nix "tree-sitter-verilog" ./tree-sitter-verilog {};
+        });
     });
-  });
-
-  hoogleAugmentedPackages = import ./nix/toggle-hoogle.nix {
-    withHoogle = withHoogle;
-    input = customHaskellPackages;
   };
 
-in
-  { tree-sitter = hoogleAugmentedPackages.tree-sitter;
-    tree-sitter-go = hoogleAugmentedPackages.tree-sitter-go;
-    tree-sitter-haskell = hoogleAugmentedPackages.tree-sitter-haskell;
-    tree-sitter-java = hoogleAugmentedPackages.tree-sitter-java;
-    tree-sitter-json = hoogleAugmentedPackages.tree-sitter-json;
-    tree-sitter-nix = hoogleAugmentedPackages.tree-sitter-nix;
-    tree-sitter-php = hoogleAugmentedPackages.tree-sitter-php;
-    tree-sitter-python = hoogleAugmentedPackages.tree-sitter-python;
-    tree-sitter-ruby = hoogleAugmentedPackages.tree-sitter-ruby;
-    tree-sitter-tsx = hoogleAugmentedPackages.tree-sitter-tsx;
-    tree-sitter-typescript = hoogleAugmentedPackages.tree-sitter-typescript;
-    tree-sitter-verilog = hoogleAugmentedPackages.tree-sitter-verilog;
-  }
+  hoogle = final: prev: {
+    haskellPackages =
+      if withHoogle
+      then prev.haskellPackages.override (old: {
+        overrides = final.lib.composeExtensions
+          (old.overrides or (_: _: {}))
+          (self: super: {
+            ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+            ghcWithPackages = self.ghc.withPackages;
+          });
+      })
+      else
+        prev.haskellPackages;
+  };
+
+  pkgs = import sources.nixpkgs { overlays = [ packages hoogle ]; };
+
+in {
+  tree-sitter = pkgs.haskellPackages.tree-sitter;
+  tree-sitter-go = pkgs.haskellPackages.tree-sitter-go;
+  tree-sitter-haskell = pkgs.haskellPackages.tree-sitter-haskell;
+  tree-sitter-java = pkgs.haskellPackages.tree-sitter-java;
+  tree-sitter-json = pkgs.haskellPackages.tree-sitter-json;
+  tree-sitter-nix = pkgs.haskellPackages.tree-sitter-nix;
+  tree-sitter-php = pkgs.haskellPackages.tree-sitter-php;
+  tree-sitter-python = pkgs.haskellPackages.tree-sitter-python;
+  tree-sitter-ruby = pkgs.haskellPackages.tree-sitter-ruby;
+  tree-sitter-tsx = pkgs.haskellPackages.tree-sitter-tsx;
+  tree-sitter-typescript = pkgs.haskellPackages.tree-sitter-typescript;
+  tree-sitter-ql = pkgs.haskellPackages.tree-sitter-ql;
+  tree-sitter-verilog = pkgs.haskellPackages.tree-sitter-verilog;
+}
--- shell.nix
+++ shell.nix
@@ -1,17 +1,18 @@
+{ sources ? import ./nix/sources.json
+, withHoogle ? false
+}:
+
 let
-  ghcPtr = import ./nix/nix-ghc-ptr.nix;
-  pinnedPkgs = ghcPtr.pinnedPkgs;
-  myPackages = (import ./release.nix { withHoogle = true; } );
-  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-  # ghcide = import (fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/ghc881") {};
+  pkgs = import sources.nixpkgs {};
+  myPackages = (import ./release.nix { inherit sources; withHoogle = true; } );
+  ghcide = import sources.ghcide {};
 
   projectDrvEnv = myPackages.tree-sitter.env.overrideAttrs (oldAttrs: rec {
     buildInputs = oldAttrs.buildInputs ++ [
-      pinnedPkgs.haskellPackages.cabal-install
-      pinnedPkgs.haskellPackages.hlint
-      pinnedPkgs.haskellPackages.ghcid
-      all-hies.versions.${ghcPtr.hieVer}
-      # ghcide.ghcide-ghc865
+      pkgs.haskellPackages.cabal-install
+      pkgs.haskellPackages.hlint
+      pkgs.haskellPackages.ghcid
+      ghcide.ghcide-ghc865
       ];
     shellHook = ''
       export HIE_HOOGLE_DATABASE="$NIX_GHC_LIBDIR/../../share/doc/hoogle/index.html"
