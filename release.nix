ad718a4b1bae4cf96c9baaf07674b315f9c697d4
Jakub Kopański <jakub@famisoft.pl>
2020-03-21 10:59:44 +0100

    Build all components upon release

--- release.nix
+++ release.nix
@@ -5,17 +5,17 @@ let
   hsPkgs = import ./default.nix { inherit sources; };
 
 in {
-  tree-sitter = hsPkgs.tree-sitter;
-  tree-sitter-go = hsPkgs.tree-sitter-go;
-  tree-sitter-haskell = hsPkgs.tree-sitter-haskell;
-  tree-sitter-java = hsPkgs.tree-sitter-java;
-  tree-sitter-json = hsPkgs.tree-sitter-json;
-  tree-sitter-nix = hsPkgs.tree-sitter-nix;
-  tree-sitter-php = hsPkgs.tree-sitter-php;
-  tree-sitter-python = hsPkgs.tree-sitter-python;
-  tree-sitter-ruby = hsPkgs.tree-sitter-ruby;
-  tree-sitter-tsx = hsPkgs.tree-sitter-tsx;
-  tree-sitter-typescript = hsPkgs.tree-sitter-typescript;
-  tree-sitter-ql = hsPkgs.tree-sitter-ql;
-  tree-sitter-verilog = hsPkgs.tree-sitter-verilog;
+  tree-sitter = hsPkgs.tree-sitter.components.all;
+  tree-sitter-go = hsPkgs.tree-sitter-go.components.all;
+  tree-sitter-haskell = hsPkgs.tree-sitter-haskell.components.all;
+  tree-sitter-java = hsPkgs.tree-sitter-java.components.all;
+  tree-sitter-json = hsPkgs.tree-sitter-json.components.all;
+  tree-sitter-nix = hsPkgs.tree-sitter-nix.components.all;
+  tree-sitter-php = hsPkgs.tree-sitter-php.components.all;
+  tree-sitter-python = hsPkgs.tree-sitter-python.components.all;
+  tree-sitter-ruby = hsPkgs.tree-sitter-ruby.components.all;
+  tree-sitter-tsx = hsPkgs.tree-sitter-tsx.components.all;
+  tree-sitter-typescript = hsPkgs.tree-sitter-typescript.components.all;
+  tree-sitter-ql = hsPkgs.tree-sitter-ql.components.all;
+  tree-sitter-verilog = hsPkgs.tree-sitter-verilog.components.all;
 }
